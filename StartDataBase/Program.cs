ab80d3e9346546f515e66207b0be231c6f929142
Yurii Korolchuk <yura.korolchuk82@gmail.com>
2020-05-22 14:22:14 +0300

    first commit: created database

---
+++ StartDataBase/Program.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Dependencies;
+using DAL.Models;
+using DAL.Repository;
+using Ninject;
+
+namespace StartDataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel(new DataAccessModule());
+
+            var data = kernel.Get<IUnitOfWork>();
+
+            data.Users.Create(new User { Login = "Admin", Pass = "Admin", RoleName = "Admin" });
+            data.Users.Create(new User { Login = "Manager", Pass = "Manager", RoleName = "Manager" });
+            data.Users.Create(new User { Login = "User", Pass = "User", RoleName = "User" });
+            data.Users.Create(new User { Login = "Guest", Pass = "Guest", RoleName = "Guest" });
+            data.Save();
+
+            data.Services.Create(new Service { Name = "ServiceName", Price = "500", Type = "ServiceForSomething" });
+            data.Save();
+        }
+    }
+}
